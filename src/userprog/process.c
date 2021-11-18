f7df2c048a8aae81550632caef98d0318d22f7f2
Ranley123 <hy30@st-andrews.ac.uk>
2021-11-18 21:14:20 +0800

    type

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -47,10 +47,10 @@ process_execute (const char *file_name)
 
   // Modify: separate the user program file name from arguments
   char * saveptr;
-  char * file_name = strtok_r((char *)file_name, " ", &saveptr);
+  char * name = strtok_r((char *)file_name, " ", &saveptr);
 
   /* Create a new thread to execute FILE_NAME. */
-  tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
+  tid = thread_create (name, PRI_DEFAULT, start_process, fn_copy);
 
   /* If we're unable to create the thread, then free its pages and exit. */
   if (tid == TID_ERROR)
